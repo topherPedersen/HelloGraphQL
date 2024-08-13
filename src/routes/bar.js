6d5dc360ce91e4a82c7f29d0874d3ede0d4987a5
Chris Pedersen <chrispedatx@gmail.com>
2024-08-12 23:38:03 -0500

    add apollo code to bar.js

--- src/routes/bar.js
+++ src/routes/bar.js
@@ -1,7 +1,31 @@
+import React, { useEffect } from 'react';
+import { client } from '../index';
+import { gql } from '@apollo/client';
+
+// Apollo GraphQL Client Quick Start Guide: https://www.apollographql.com/docs/react/get-started
+
 export default function Bar() {
-    return (
-      <div>
-        <h1>Bar Page</h1>
-      </div>
-    );
-  }
+  useEffect(() => {
+    client
+      // TODO: Try and query spotify graphql client instead of the playground client from Apollo
+      .query({
+        query: gql`
+          query GetLocations {
+            locations {
+              id
+              name
+              description
+              photo
+            }
+          }
+        `,
+      })
+      .then((result) => alert(JSON.stringify(result, null, 2)));
+  }, []);
+
+  return (
+    <div>
+      <h1>Bar Page</h1>
+    </div>
+  );
+}
