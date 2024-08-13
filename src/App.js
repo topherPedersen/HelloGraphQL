3634f465a2c1708e0ff2239464846e4e62a242c9
Chris Pedersen <chrispedatx@gmail.com>
2024-08-12 22:42:28 -0500

    install react router, begin adding boilerplate from react router quickstart guide

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
+      {/* TODO: Is App.js still used after installing React Router? Delete if no longer needed. */}
       <h1>KGQL: GraphQL Radio</h1>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Root from "./routes/root";
 import reportWebVitals from './reportWebVitals';
+import ErrorPage from "./error-page";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// React Router Quick Start Guide: https://reactrouter.com/en/main/start/tutorial
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
